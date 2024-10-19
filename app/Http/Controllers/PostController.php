e812a3e5a6868550365f5e070c7c7a845821906d
Marlon2025c <bastien.lecour2025@gmail>
2024-10-19 23:49:14 +0200

    💄

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -10,7 +10,7 @@ class PostController extends Controller
     public function index(){
 
         return view('home', [
-            'ville' == Ville::all()
+            'ville' == ville::all()
         ]);
     }
 }
--- app/Models/Ville.php
+++ app/Models/Ville.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Ville extends Model 
+class ville extends Model 
 {
     use HasFactory;
 }
