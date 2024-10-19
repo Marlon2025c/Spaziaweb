847802db102df41d6df378d1810abb9e92ba4403
Marlon2025c <bastien.lecour2025@gmail>
2024-10-19 23:48:23 +0200

    ♻️

--- app/Models/Ville.php
+++ app/Models/Ville.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Notation extends Model 
+class Ville extends Model 
 {
     use HasFactory;
 }
