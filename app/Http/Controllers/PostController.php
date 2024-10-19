0f62e341e50bddfc5e1d8510d3f3ef1ec20508a1
Marlon2025c <bastien.lecour2025@gmail>
2024-10-19 23:50:00 +0200

    📝

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -10,7 +10,7 @@ class PostController extends Controller
     public function index(){
 
         return view('home', [
-            'ville' == ville::all()
+            'ville' => ville::all()
         ]);
     }
 }
