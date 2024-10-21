e01de43764cbf797d49422e4c854f2a57d8576a1
Marlon2025c <bastien.lecour2025@gmail.com>
2024-10-21 09:50:49 +0200

    🚧 debug bdd 🚧

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,12 +2,13 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Notation;
+use App\Models\Ville;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    public function index(){
+    public function index()
+    {
 
         return view('home', [
             'ville' => Ville::all()
--- app/Models/Ville.php
+++ app/Models/Ville.php
@@ -5,9 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Ville extends Model 
+class Ville extends Model // Ville avec un "V" majuscule
 {
     use HasFactory;
 }
-
-?>
