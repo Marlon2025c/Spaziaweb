2136dac35cf012cfa90f52045d43b1f7a0cd2201
Marlon2025c <bastien.lecour2025@gmail.com>
2024-10-21 11:26:25 +0200

    🚧

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,49 +5,50 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
     <title>Document</title>
 </head>
 
 <body>
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#">Navbar</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
-                aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNavDropdown">
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Accueil</a>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
-                            aria-expanded="false">
-                            Communauté
-                        </a>
-                        <ul class="dropdown-menu">
-                            <li><a class="dropdown-item" href="#">Notations</a></li>
-                            <li><a class="dropdown-item" href="#">Patch Notes</a></li>
-                            <li><a class="dropdown-item" href="#">Event</a></li>
-                        </ul>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Voter</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">WIKI</a>
-                    </li>
-                </ul>
-            </div>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <a class="navbar-brand" href="#">
+                    <img src="https://uiwjs.github.io/bootstrap-icons/index.html" alt="Logo" width="30" height="24" class="d-inline-block align-text-top">
+                    Bootstrap
+                </a>
+            </ul>
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link nav-button" aria-current="page" href="#"><h6>Accueil</h6></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#"><h6 class="b">Notations</h6></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#"><h6>Jornal</h6></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#"><h6>Wiki</h6></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#"><h6>Vote</h6></a>
+                </li>
+            </ul>
+          </div>
         </div>
-    </nav>
+      </nav>
 </body>
 
 </html>
-@foreach ($villes as $villes)
+{{-- @foreach ($villes as $villes)
     <h4>{{ $villes['nom_ville'] }}</h4>
-@endforeach
+@endforeach --}}
+
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
     integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
 </script>
