d48e0042bbf62f5257eacab0f9e5c22a183ba4aa
Marlon2025c <bastien.lecour2025@gmail.com>
2024-10-21 11:27:47 +0200

    💚

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,43 +5,56 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('resources/css/style.css') }}">
     <title>Document</title>
 </head>
 
 <body>
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
         <div class="container-fluid">
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <a class="navbar-brand" href="#">
-                    <img src="https://uiwjs.github.io/bootstrap-icons/index.html" alt="Logo" width="30" height="24" class="d-inline-block align-text-top">
-                    Bootstrap
-                </a>
-            </ul>
-            <ul class="navbar-nav">
-                <li class="nav-item">
-                    <a class="nav-link nav-button" aria-current="page" href="#"><h6>Accueil</h6></a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#"><h6 class="b">Notations</h6></a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#"><h6>Jornal</h6></a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#"><h6>Wiki</h6></a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#"><h6>Vote</h6></a>
-                </li>
-            </ul>
-          </div>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <a class="navbar-brand" href="#">
+                        <img src="https://uiwjs.github.io/bootstrap-icons/index.html" alt="Logo" width="30"
+                            height="24" class="d-inline-block align-text-top">
+                        Bootstrap
+                    </a>
+                </ul>
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link nav-button" aria-current="page" href="#">
+                            <h6>Accueil</h6>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">
+                            <h6 class="b">Notations</h6>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">
+                            <h6>Jornal</h6>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">
+                            <h6>Wiki</h6>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">
+                            <h6>Vote</h6>
+                        </a>
+                    </li>
+                </ul>
+            </div>
         </div>
-      </nav>
+    </nav>
 </body>
 
 </html>
