ceda4342e0cde5c706f35fa8f88449cea278b5f7
Marlon2025c <bastien.lecour2025@gmail>
2024-10-21 18:55:37 +0200

    🚧 Ajoute de VC Notations🚧

--- routes/web.php
+++ routes/web.php
@@ -3,3 +3,4 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [\App\Http\Controllers\PostController::class, 'index']);
+Route::get('/notations', [\App\Http\Controllers\Notations::class, 'index']);
