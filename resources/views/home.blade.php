d13e165881b608d22e3fea53555d0fdac2d09e43
Marlon2025c <bastien.lecour2025@gmail.com>
2024-10-22 09:28:33 +0200

    🚧 double navbar 🚧

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,57 +14,106 @@
 </style>
 
 <body>
-    <nav class="navbar navbar-expand-lg fixed-top">
-        <div class="container-fluid">
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
-                aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item nav-li-textsp">
-                        <a class="navbar-brand nav-ul-a p-1" href="#">
-                            <img src="{{ asset('img/spaziaeco logo.png') }}" alt="Logo" height="60"
-                                width="auto" class="rounded">
-                            SpaziaEco
-                        </a>
-                    </li>
-                </ul>
-                <ul class="navbar-nav">
-                    <li class="nav-li-textsp">
-                        <a class="nav-link text-uppercase  nav-ul-a p-2" aria-current="page" href="#">
-                            <h6 class="m-0">Accueil</h6>
-                        </a>
-                    </li>
-                    <li class="nav-li-textsp">
-                        <a class="nav-link text-uppercase nav-ul-a p-2" href="#">
-                            <h6 class="m-0">Notations</h6>
-                        </a>
-                    </li>
-                    <li class="nav-li-textsp">
-                        <a class="nav-link text-uppercase  nav-ul-a p-2" href="#">
-                            <h6 class="m-0">Jornal</h6>
-                        </a>
-                    </li>
-                    <li class="nav-li-textsp">
-                        <a class="nav-link text-uppercase nav-ul-a p-2" href="#">
-                            <h6 class="m-0">Wiki</h6>
-                        </a>
-                    </li>
-                    <li class="nav-li-textsp">
-                        <a class="nav-link text-uppercase nav-ul-a p-2" href="#">
-                            <h6 class="m-0">Vote</h6>
-                        </a>
-                    </li>
-                </ul>
+    <div class="fixed-top">
+        <nav class="navbar navbar-expand-lg p-0">
+            <div class="container-fluid">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                    aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <!--<li class="nav-item nav-li-textsp">
+                            <a class="navbar-brand nav-ul-a p-1" href="#">
+                                <img src="{{ asset('img/spaziaeco logo.png') }}" alt="Logo" height="60"
+                                    width="auto" class="rounded">
+                                Radio
+                            </a>
+                        </li>-->
+                    </ul>
+                    <ul class="navbar-nav">
+                        <li class="nav-li-textsp">
+                            <a class="nav-link  nav-ul-a p-2" aria-current="page" href="#">
+                                <p class="fs-6 text-capitalize m-0">Support</p>
+                                <!-- style="font-size: 15px" -->
+                            </a>
+                        </li>
+                        <li class="nav-li-textsp">
+                            <a class="nav-link nav-ul-a p-2" href="#">
+                                <p class="fs-6 text-capitalize m-0">Discord</p>
+                            </a>
+                        </li>
+                        <li class="nav-li-textsp">
+                            <a class="nav-link nav-ul-a p-2" href="#">
+                                <p class="fs-6 text-capitalize m-0">Connexion / Inscription</p>
+                            </a>
+                        </li>
+                        <li class="nav-li-textsp">
+                            <a class="nav-link nav-ul-a p-2" href="#">
+                                <p class="fs-6 text-capitalize m-0">Parametre</p>
+                            </a>
+                        </li>
+                        <li class="nav-li-textsp">
+                            <a class="nav-link nav-ul-a p-2" href="#">
+                                <p class="fs-6 text-capitalize m-0">Vote</p>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
             </div>
-        </div>
-    </nav>
-
+        </nav>
+        <nav class="navbar navbar-expand-lg">
+            <div class="container-fluid">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                    aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item nav-li-textsp">
+                            <a class="navbar-brand nav-ul-a p-1" href="#">
+                                <img src="{{ asset('img/spaziaeco logo.png') }}" alt="Logo" height="60"
+                                    width="auto" class="rounded">
+                                SpaziaEco
+                            </a>
+                        </li>
+                    </ul>
+                    <ul class="navbar-nav">
+                        <li class="nav-li-textsp">
+                            <a class="nav-link text-uppercase  nav-ul-a p-2" aria-current="page" href="#">
+                                <h6 class="m-0">Accueil</h6>
+                            </a>
+                        </li>
+                        <li class="nav-li-textsp">
+                            <a class="nav-link text-uppercase nav-ul-a p-2" href="#">
+                                <h6 class="m-0">Notations</h6>
+                            </a>
+                        </li>
+                        <li class="nav-li-textsp">
+                            <a class="nav-link text-uppercase  nav-ul-a p-2" href="#">
+                                <h6 class="m-0">Jornal</h6>
+                            </a>
+                        </li>
+                        <li class="nav-li-textsp">
+                            <a class="nav-link text-uppercase nav-ul-a p-2" href="#">
+                                <h6 class="m-0">Wiki</h6>
+                            </a>
+                        </li>
+                        <li class="nav-li-textsp">
+                            <a class="nav-link text-uppercase nav-ul-a p-2" href="#">
+                                <h6 class="m-0">Vote</h6>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </div>
     <div>
         <img class="img-fluid" src="{{ asset('img/spazia.png') }}" alt="">
-        // http://localhost/Spaziaweb/public/img/spazia.png
+        <!-- http://localhost/Spaziaweb/public/img/spazia.png -->
         <h1>Bienvenue sur Mon Site</h1>
         <p>Profitez de nos fonctionnalités exclusives !</p>
     </div>
