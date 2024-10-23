dfbd122f0d02ea8cb75a9225ad595e8a9ef485c3
Marlon2025c <bastien.lecour2025@gmail.com>
2024-10-23 11:35:38 +0200

    🚧 style et annimation css🚧

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,37 +11,7 @@
 
 </head>
 <style>
-    .nav-ul-a {
-        color: white;
-    }
 
-    .nav-li-textsp:hover {
-        background: rgb(148, 148, 148, 0.3);
-        color: white;
-    }
-
-    .navbar {
-        background-color: #0e0e0e45;
-        transition: background-color 0.3s;
-        /* Transition pour un changement en douceur */
-    }
-
-    .navbar-header {
-        background-color: rgba(0, 0, 0, 1);
-        transition: background-color 0.3s;
-        /* Transition pour un changement en douceur */
-    }
-
-    .fixed-top {
-        transition: background-color 0.3s;
-        /* Transition pour le changement de couleur */
-    }
-
-    /* Classe pour la navbar avec une couleur de fond opaque */
-    .navbar-scrolled {
-        background-color: #101015;
-        /* Changez la couleur comme vous le souhaitez */
-    }
 </style>
 
 <body>
@@ -60,7 +30,13 @@
                             </a>
                         </li>
                         <li class="list-group-item nav-li-textsp">
-                            <a class="fw-bold link-underline-dark nav-ul-a btn btn-sm p-2" href="#">
+                            <a class="fw-bold link-underline-dark nav-ul-a btn btn-sm p-2 d-flex align-items-center justify-content-center shakeme"
+                                href="#">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+                                    fill="currentColor" class="bi bi-discord svg-icon me-1" viewBox="0 0 16 16">
+                                    <path
+                                        d="M13.545 2.907a13.2 13.2 0 0 0-3.257-1.011.05.05 0 0 0-.052.025c-.141.25-.297.577-.406.833a12.2 12.2 0 0 0-3.658 0 8 8 0 0 0-.412-.833.05.05 0 0 0-.052-.025c-1.125.194-2.22.534-3.257 1.011a.04.04 0 0 0-.021.018C.356 6.024-.213 9.047.066 12.032q.003.022.021.037a13.3 13.3 0 0 0 3.995 2.02.05.05 0 0 0 .056-.019q.463-.63.818-1.329a.05.05 0 0 0-.01-.059l-.018-.011a9 9 0 0 1-1.248-.595.05.05 0 0 1-.02-.066l.015-.019q.127-.095.248-.195a.05.05 0 0 1 .051-.007c2.619 1.196 5.454 1.196 8.041 0a.05.05 0 0 1 .053.007q.121.1.248.195a.05.05 0 0 1-.004.085 8 8 0 0 1-1.249.594.05.05 0 0 0-.03.03.05.05 0 0 0 .003.041c.24.465.515.909.817 1.329a.05.05 0 0 0 .056.019 13.2 13.2 0 0 0 4.001-2.02.05.05 0 0 0 .021-.037c.334-3.451-.559-6.449-2.366-9.106a.03.03 0 0 0-.02-.019m-8.198 7.307c-.789 0-1.438-.724-1.438-1.612s.637-1.613 1.438-1.613c.807 0 1.45.73 1.438 1.613 0 .888-.637 1.612-1.438 1.612m5.316 0c-.788 0-1.438-.724-1.438-1.612s.637-1.613 1.438-1.613c.807 0 1.451.73 1.438 1.613 0 .888-.631 1.612-1.438 1.612" />
+                                </svg>
                                 Discord
                             </a>
                         </li>
