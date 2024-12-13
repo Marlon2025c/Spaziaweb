878d77ac20ec13bc296c90d504e839bcb7489cec
Marlon2025c <bastien.lecour2025@gmail>
2024-12-13 21:46:12 +0100

    📝

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\EverGardenController;
 
 Route::get('/', [\App\Http\Controllers\PostController::class, 'index']);
 Route::get('/notations', [\App\Http\Controllers\Notations::class, 'index']);
