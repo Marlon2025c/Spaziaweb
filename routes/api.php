333514df7d67de9e886c8a3724863143b1ed778a
Marlon2025c <bastien.lecour2025@gmail>
2024-12-13 22:55:17 +0100

    🚨

--- routes/api.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-use App\Http\Controllers\ModLogController;
-
-Route::post('/mod-logs', [ModLogController::class, 'store']);
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,6 @@ use App\Http\Controllers\EverGardenController;
 
 Route::get('/', [\App\Http\Controllers\PostController::class, 'index']);
 Route::get('/notations', [\App\Http\Controllers\Notations::class, 'index']);
+use App\Http\Controllers\ModLogController;
+
+Route::post('/mod-logs', [ModLogController::class, 'store']);
