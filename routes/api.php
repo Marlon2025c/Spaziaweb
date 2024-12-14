d144073208f5d8a03d8f72478e4f453f08b9388d
Marlon2025c <bastien.lecour2025@gmail>
2024-12-14 12:24:12 +0100

    💚

--- routes/api.php
+++ routes/api.php
@@ -1,14 +1,4 @@
 <?php
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ApiController;
 
-Route::post('/receive-data', function (Request $request) {
-    // Récupérer les données JSON envoyées
-    $data = $request->all();
-
-    // Par exemple, les enregistrer dans un fichier ou une base de données
-    \Log::info($data);
-
-    // Retourner une réponse JSON
-    return response()->json(['status' => 'success', 'message' => 'Données reçues']);
-});
+Route::middleware([])->get('/example', [ApiController::class, 'example']);
